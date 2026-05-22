6c8d35efdf422904bbcd539c7944cb48d39cbca7
Glurbl <matteswinter37@gmail.com>
2026-05-22 18:41:53 +0200

    U3 mit erklärung

--- U3.hs
+++ U3.hs
@@ -1,3 +1,4 @@
+{- HLINT ignore "Use camelCase" -}
 import Data.List (sort)
 
 data Prozess = Prozess { pid :: String
@@ -26,7 +27,7 @@ idle = Prozess { pid = "IDLE", arrival = -1, computing = -1 }
 --Test ohne show mit "print start" ausgabe funktionert ist aber super unübersichtlich, deshalb Aufgabe 1 
 
 
-ps :: [Prozess]
+ps :: [Prozess]                                                 --Testeingabe 
 ps = [ Prozess { pid = "P1", arrival = 0,  computing = 6 }
      , Prozess { pid = "P2", arrival = 2,  computing = 6 }
      , Prozess { pid = "P3", arrival = 4,  computing = 5 }
@@ -37,12 +38,12 @@ ps = [ Prozess { pid = "P1", arrival = 0,  computing = 6 }
 start :: State
 start = State { new = ps, run = idle, ready = [], time = 0, chart = "" }
 
---Aufgabe 1 --anstelle von "deriving (Show)" definieren wir selber wie "State" ausgegeben wird
-instance Show State where
-    show s = unlines 
-        ([ "-- new" ]
-        ++ map show (new s)
-        ++ [ "-- run" ]
+--Aufgabe 1 "print start"
+instance Show State where   --anstelle von "deriving (Show)" definieren wir selber wie "State" ausgegeben wird
+    show s = unlines        --s ist State der ausgegeben wird, unlines verbindet liste von strings mit zeilenumbrüchen
+        ([ "-- new" ]          --Überschriften der Prozesse 
+        ++ map show (new s)    -- ++ hängt es and die Liste, map show wandelt Prozess in String um, (new s) nimmt Liste der noch nicht angekommenen Prozesse aus dem "State" 
+        ++ [ "-- run" ]         --eckige Klammern, da unlines eine Liste erwartet !
         ++ [ show (run s) ]
         ++ [ "-- ready" ]
         ++ map show (ready s)
@@ -52,45 +53,47 @@ instance Show State where
 
     --Aufgabe 2 
     --Hilfsfunktion
-splitByTime :: Int -> [Prozess] -> ([Prozess], [Prozess])
-splitByTime t [] = ([], [])
+splitByTime :: Int -> [Prozess] -> ([Prozess], [Prozess])   --nimmt int und Liste und gibt Tupel mit 2 Listen zurück -> Prozesse die jetzt ankommen und Prozesse die noch nicht da sind
+splitByTime t [] = ([], []) --basisfall für leere Liste
 splitByTime t (x:xs)
-    | arrival x == t = (x : angekommen, nichtDa)
+    | arrival x == t = (x : angekommen, nichtDa)    --prüft ob x ankommt, falls ja x in ankommen sonst x in nichtda
     | otherwise      = (angekommen, x : nichtDa)
-    where (angekommen, nichtDa) = splitByTime t xs
+    where (angekommen, nichtDa) = splitByTime t xs  --ruft Splitbytime rekursiv für den rest von xs auf 
 
     --Hauptfunktion
-update_ready :: State -> State
-update_ready s = s { new   = nichtDa
+update_ready :: State -> State      --funktion nimmt State und gibt aktualisierten State zurück, print (update_ready start)-> P1 wurde in ready verschoben
+update_ready s = s { new   = nichtDa        --kopiert State s und ersetzt new und ready 
                    , ready = ready s ++ angekommen }
-    where (angekommen, nichtDa) = splitByTime (time s) (new s)
+    where (angekommen, nichtDa) = splitByTime (time s) (new s)  --where definiert Hilfsvariable für diese Funktion und gibt einen Tupel zurück, splitBytime entpackt den Tupel in die beiden Variablen
 
 --Aufgabe 3             
-update_run :: State -> State
+update_run :: State -> State    --gibt erneut aktualisierten State zurück
 update_run s
-    | pid (run s) == "IDLE" && not (null (ready s)) =
-        s { run   = head (ready s)
-          , ready = tail (ready s) }
-    | otherwise = s
+    | pid (run s) == "IDLE" && not (null (ready s)) =   --prüft, dass gerade kein Prozess läuft und das liste nicht leer ist -> not
+        s { run   = head (ready s)  --nimmt ersten Prozess aus ready -> wird laufender Prozess
+          , ready = tail (ready s) }    --entfernt ersten Prozess aus ready 
+    | otherwise = s                     -- falls && nicht greift bleibt State unverändert
+    {-bsp ready = [P1, P2, P3]
 
-                {-zum starten Schritt 1: update_ready testen
+    head [P1, P2, P3] = P1      -- P1 wird run
+    tail [P1, P2, P3] = [P2, P3] -- P2, P3 bleiben in ready -}
+
+                {-zum starten 
                          let s1 = update_ready start
+                         print (update_run s1)
 
-                Schritt 2: update_run darauf anwenden
-                        let s2 = update_run s1
-                Ausgeben
-                      print s2-}
+                -}
 
 --Aufgabe 4
-update_time :: State -> State
-update_time s = s { run   = neuerRun
-                  , time  = time s + 1
-                  , chart = chart s ++ pid (run s) ++ " " }
+update_time :: State -> State   
+update_time s = s { run   = neuerRun        --laufender Prozess wird durch neuerRun ersetzt 
+                  , time  = time s + 1      --aktuelle Zeit um 1 erhöht
+                  , chart = chart s ++ pid (run s) ++ " " }  --chart s scheduling String pid(run s) holt laufenden Prozess
     where
-        neuzeit  = computing (run s) - 1
+        neuzeit  = computing (run s) - 1    --rechenzeit des laufenden Prozesses wird um 1 reduziert 
         neuerRun
-            | neuzeit == 0 = idle
-            | otherwise    = (run s) { computing = neuzeit }
+            | neuzeit == 0 = idle       --wenn rechenzeit 0 -> idle
+            | otherwise    = (run s) { computing = neuzeit }    --sonst Prozess weiter mit neuer rechenzeit
             {-Ausführen: let s1 = update_ready start
             let s2 = update_run s1
             let s3 = update_time s2
@@ -98,9 +101,8 @@ update_time s = s { run   = neuerRun
 
 --Aufgabe 5
 
--- Ein einzelner Schritt
 step :: State -> State
-step s = (update_time . update_run) (update_ready s)
+step s = update_time (update_run (update_ready s))   -- verbindet funktionen, von rechts nach links
 
 -- Prüft ob alle Prozesse fertig sind
 fertig :: State -> Bool
@@ -112,13 +114,23 @@ fertig s = null (new s)
 fcfs :: State -> State
 fcfs s
     | fertig s  = s
-    | otherwise = fcfs (step s) --Ausführen putStrLn (chart (fcfs start))
+    | otherwise = fcfs (step s) --Ausführen print (fcfs start)
+                                
 
---Aufgabe 7
+--Aufgabe 6
 update_ready_sjf :: State -> State
 update_ready_sjf s = s { new   = nichtDa
-                       , ready = sort (ready s ++ angekommen) }
+                       , ready = sort (ready s ++ angekommen) } --für shortest job First wird die Liste sortiert angehängt
     where (angekommen, nichtDa) = splitByTime (time s) (new s)
+
+
+step_sjf :: State -> State  --rest ist wie in aufgabe 5
+step_sjf s = update_time  (update_run  (update_ready_sjf s))
+
+sjf :: State -> State
+sjf s
+    | fertig s  = s
+    | otherwise = sjf (step_sjf s)    
 
 
 
@@ -131,23 +143,17 @@ update_ready_srtf s = s { new   = nichtDa
         (angekommen, nichtDa) = splitByTime (time s) (new s)
         alleReady  = ready s ++ angekommen
         neuerRun
-            | not (null alleReady) &&
+            | not (null alleReady) &&       -- 2bedingungen, es gibt wartende Prozesse und kürzester wartender Prozess hat weniger Rechenzeit als aktuelle -> falls beide zutreffen wir der neue Prozess ausgeführt
               head (sort alleReady) < run s = head (sort alleReady)
             | otherwise                     = run s
         neueReady
-            | not (null alleReady) &&
+            | not (null alleReady) &&           --falls unterbrochen wird, kommt Prozess zurück in ready da der kürzeste Prozess bereits als neuerRun genommen wurde, nimmt Tail ihn aus ready 
               head (sort alleReady) < run s = run s : tail (sort alleReady)
             | otherwise                     = alleReady
 
-step_sjf :: State -> State
-step_sjf = update_time . update_run . update_ready_sjf
 
-sjf :: State -> State
-sjf s
-    | fertig s  = s
-    | otherwise = sjf (step_sjf s)
 
-step_srtf :: State -> State
+step_srtf :: State -> State --print (srtf start)
 step_srtf = update_time . update_run . update_ready_srtf
 
 srtf :: State -> State
